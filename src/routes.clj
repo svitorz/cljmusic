e529df79dda5ac995390980e768415d16bd2c4d7
svitorz <vitor.fabio.castro@gmail.com>
2025-08-20 13:48:55 -0300

    feat: criar rota para inserir musicas

--- src/routes.clj
+++ src/routes.clj
@@ -1,5 +1,5 @@
 (ns routes
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [compojure.core :refer [GET POST defroutes]]
             [controller :as ctr]
             [ring.util.response :as resp]
             [compojure.route :as route]))
@@ -8,6 +8,7 @@
 
 (defroutes routes 
   (GET "/musics" [] (ctr/all-songs))
+  (POST "/musics" req (ctr/create-song req)) 
   (route/not-found (resp/not-found {:body "Page not found"}))
   )
 
