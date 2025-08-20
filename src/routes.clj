fa511009e46afa8e117593420ca4ac1387d4bc26
svitorz <vitor.fabio.castro@gmail.com>
2025-08-20 09:29:06 -0300

    feat: retornar musicas em formato json

--- src/routes.clj
+++ src/routes.clj
@@ -1,8 +1,23 @@
 (ns routes
   (:require [compojure.core :refer [GET defroutes]]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [datomic.api :as d]
+            [database :as db]
+            [cheshire.core :as json]))
+
+(def db-uri (System/getenv "DB_URI"))
+
+(defn all-songs []
+  (let [conn (d/connect db-uri)
+        db   (d/db conn)]
+    (d/q db/all-songs-q db)))
 
 (defroutes routes 
-  (GET "/musics" [] "<h1>CljMusic<h1>")
-  (route/not-found "<h1>page not found<h1>"))
+  (GET "/musics" []
+    {:status 200
+     :headers {"Content-Type" "application/json"}
+     :body (json/generate-string (all-songs))})
+  (route/not-found {:status 404 
+     :headers {"Content-Type" "application/json"}
+     :body (json/generate-string "Page not found")}))
 
