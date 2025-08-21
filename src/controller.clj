c42475ea5d80c0281ddcaaf762ca4bed5e84a6d5
svitorz <vitor.fabio.castro@gmail.com>
2025-08-20 22:45:05 -0300

    feat: implement songs create method

--- src/controller.clj
+++ src/controller.clj
@@ -4,12 +4,22 @@
             [ring.util.response :as resp]))
 
 
-(def db-uri (System/getenv "DB_URI"))
-
 (defn all-songs []
-  (let [conn (d/connect db-uri)
+  (let [conn (d/connect db/db-uri)
         db   (d/db conn)
         rows (d/q db/all-songs-q db) ]
         (-> 
         (resp/response (vec rows))
         (resp/content-type "application/json"))))
+
+(defn create-song [request]
+  (let [conn (d/connect db/db-uri)
+        title (get-in request [:params "title"])
+        artist_name (get-in request [:params "artist_name"])
+        release_date (get-in request [:params "release_date"])
+        row (d/transact conn [{:music/title title
+                            :music/artist_name artist_name
+                            :music/release_date release_date}])])
+  (->
+    (resp/response "Created.") 
+    (resp/content-type "application/json")))
